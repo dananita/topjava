60df0bec86b17ceeed7f0f86fa27e5f525bd0563
dananita <zlakdanata@yandex.ru>
2018-07-25 10:24:40 +0300

    3_2_HW03_create_and_insert_db_meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals(description, datetime, callories) VALUES
+  ('Lunch','25.07.2017',510),
+  ('lunchtime','25.07.2017',675),
+  ('dinner','25.07.2017',700);
